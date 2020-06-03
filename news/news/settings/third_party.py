5aade91d65a84a07829dd3df5765c5770d65f4ab
Oleg Mazur <real.trolforever@gmail.com>
2020-06-03 05:39:20 +0300

    lil changes

--- news/news/settings/third_party.py
+++ news/news/settings/third_party.py
@@ -8,10 +8,7 @@ REST_FRAMEWORK = {
         "rest_framework.parsers.FormParser",
         "rest_framework.parsers.MultiPartParser",
     ),
-    "DEFAULT_RENDERER_CLASSES": (
-        "rest_framework.renderers.JSONRenderer",
-        "rest_framework.renderers.BrowsableAPIRenderer",
-    ),
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
     "DEFAULT_METADATA_CLASS": "rest_framework.metadata.SimpleMetadata",
 }
 
